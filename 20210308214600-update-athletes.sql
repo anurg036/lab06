7b2eb9d3c1bd41547eac253f8bccb6e6f3ff2827
anurg036 <64798142+anurg036@users.noreply.github.com>
2021-03-08 21:39:45 -0500

    Update 20210308214600-update-athletes.sql

--- 20210308214600-update-athletes.sql
+++ 20210308214600-update-athletes.sql
@@ -2,4 +2,4 @@ ALTER TABLE "leaderboard_migrated".athletes
 RENAME COLUMN is_male TO male_athlete;
 
 INSERT INTO "leaderboard_migrated".schema_migrations (migration, migrated_at) VALUES
-('202103208214600-update-athletes.sql', '2021-03-08 21:46:00'); 
+('20210308214600-update-athletes.sql', '2021-03-08 21:46:00'); 
