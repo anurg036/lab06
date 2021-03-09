ebcb07a0ff88d834be60e48b83b6f4a8ddc441c7
anurg036 <64798142+anurg036@users.noreply.github.com>
2021-03-08 21:37:37 -0500

    Update 20210308212600-create-athletes.sql

--- 20210308212600-create-athletes.sql
+++ 20210308212600-create-athletes.sql
@@ -9,9 +9,9 @@ CREATE TABLE "leaderboard_migrated".athletes (
 INSERT INTO "leaderboard_migrated".athletes
   (athlete_id, full_name, birthday, is_male)
 VALUES
-  (1, 'Alexandre Pineau', '06-14-2000', true),
-  (2, 'Robert Naim', '05-13-1998', true),
-  (3, 'Pierre Rofail', '12-05-1960', true),
-  (4, 'CaitLin McNaughton', '07-28-2001', false),
-  (5, 'Alyssa Rose', '12-31-1999', false),
-  (6, 'Michelle Beaulieu', '10-01-1980', false);
+  (1, 'bora Nurgoren', '04-12-2000', true),
+  (2, 'Ali Goralı', '06-12-1999', true),
+  (3, 'Fenasi Rotacı', '03-12-1970', true),
+  (4, 'Bomba Yapma', '02-09-2001', false),
+  (5, 'Ferhat Ibne', '04-24-1999', false),
+  (6, 'Hasan Sever', '12-01-1980', false);
